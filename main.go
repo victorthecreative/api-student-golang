d5a4152e656d65811a2e2154be8e5cdf5fecfa86
Victor Coelho <teste@192.168.68.116>
2024-06-17 23:00:12 -0300

    Create all routes for server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -15,12 +16,37 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/students", getStudent)
-
+	e.GET("/students", getStudents)
+	e.POST("/students", createStudent)
+	e.GET("/student/:id", getStudent)
+	e.PUT("/students/:id", updateStudent)
+	e.DELETE("/students/:id", deleteStudent)
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func getStudent(c echo.Context) error {
+func getStudents(c echo.Context) error {
 	return c.String(http.StatusOK, "List of all students")
+}
+
+func createStudent(c echo.Context) error {
+	return c.String(http.StatusOK, "Create student")
+}
+
+func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	GetStud := fmt.Sprintf("Get %s student", id)
+	return c.String(http.StatusOK, GetStud)
+}
+
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	UpdateStud := fmt.Sprintf("Update %s student", id)
+	return c.String(http.StatusOK, UpdateStud)
+}
+
+func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	DeleteStud := fmt.Sprintf("Delete %s student", id)
+	return c.String(http.StatusOK, DeleteStud)
 }
